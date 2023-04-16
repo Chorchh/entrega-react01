8fb16f2e2dd22d03710dec6d7c47930f669dfa60
Chorchh <jorcapai@gmail.com>
2023-04-15 21:36:19 -0300

    entrega3

--- src/pages/pokeAPI/PokeApi.jsx
+++ src/pages/pokeAPI/PokeApi.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { ButtonPokemon, Container, FormPokemon, InputPokemon, TituloPokedex } from './pokeApiStyles';
+import { ButtonPokemon, Container, ContainerPokemon, DataPokemon, FormPokemon, HabilidadPokemon, ImagenPokemon, InputPokemon, NombrePokemon, TituloPokedex } from './pokeApiStyles';
 
 
 
 const PokeApi = () => {
-  const [pokemon, setPokemon] = useState([])
   const [inputNumber, setInputNumber] = useState("")
+  const [pokemon, setPokemon] = useState([])
+  const [weight, setWeight] = useState([])
+  const [height, setHeight] = useState([])
+
 
   const handleChange = (e) => setInputNumber(e.target.value)
   const URL = `https://pokeapi.co/api/v2/pokemon/${inputNumber}`
@@ -16,8 +19,22 @@ const PokeApi = () => {
   useEffect(() => {
     axios.get(URL).then((res) => {
       setPokemon(res.data.name)
+      setWeight(res.data.weight)
+      setHeight(res.data.height)
     })
   }, [URL])
+
+
+  const altura = altura => {
+    let medida = altura / 10
+    return medida
+  }
+
+  const peso = peso => {
+    let medida = peso / 10
+    return medida
+  }
+
 
   return (
     <>
@@ -26,7 +43,12 @@ const PokeApi = () => {
         <FormPokemon>
           <InputPokemon type='number' placeholder='Ingresa un número de Pokemon' value={inputNumber} onChange={handleChange}/>
           <ButtonPokemon>Buscar</ButtonPokemon>
-          <p style={{color: "white"}}>Nombre: {pokemon}</p>
+          <ContainerPokemon>
+            <NombrePokemon>{pokemon}</NombrePokemon>
+            <ImagenPokemon src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inputNumber}.png`} alt="pokemon"/>
+            <DataPokemon>Peso: {altura(weight)} Mts</DataPokemon>
+            <DataPokemon>Altura: {peso(height) } Kgs</DataPokemon>
+          </ContainerPokemon>
         </FormPokemon>
       </Container>
     </>
--- src/pages/pokeAPI/pokeApiStyles.js
+++ src/pages/pokeAPI/pokeApiStyles.js
@@ -41,4 +41,48 @@ export const ButtonPokemon = styled.button`
   border-radius: 10px;
   border: none;
   background-color: red;
+  margin-top: 10px;
+`;
+
+export const ContainerPokemon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 300px;
+  height: 400px;
+  border: 1px solid red;
+  background-color: white;
+  padding: 10px;
+  margin-top: 40px;
+  border-radius: 5px;
+`;
+
+export const NombrePokemon = styled.h2`
+  font-size: 30px;
+  color: black;
+  text-align: center;
+  margin-top: 10px;
+  text-transform: uppercase;
+`;
+
+export const ImagenPokemon = styled.img`
+  width: 150px;
+  height: 150px;
+`;
+
+export const DataPokemon = styled.p`
+  font-size: 18px;
+  color: black;
+  text-align: center;
+  margin-top: 5px;
+  text-transform: capitalize;
+`;
+
+export const HabilidadPokemon = styled.p`
+  font-size: 18px;
+  color: black;
+  text-align: center;
+  margin-top: 5px;
+  text-transform: capitalize;
 `;
